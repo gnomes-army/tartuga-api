c60120be599eb488c44214b938e96cba50da7105
Oleg Babenko <majerkravchuk@gmail.com>
2020-05-07 21:43:07 +0300

    Remove AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { SessionAuthMiddleware } from './session-auth.middleware';
 import { AuthController } from './auth.controller';
 import { AuthGateway } from './auth.gateway';
@@ -8,7 +7,7 @@ import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [PassportModule],
-  providers: [AuthService, AuthGateway, GoogleStrategy],
+  providers: [AuthGateway, GoogleStrategy],
   controllers: [AuthController]
 })
 export class AuthModule implements NestModule {
