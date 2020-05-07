830f5a39fd01dbc4abb45723288865717c4361b5
Oleg Babenko <majerkravchuk@gmail.com>
2020-05-07 22:16:30 +0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const { PORT = 3000 } = process.env;
-
-  Logger.log(`Env PORT: ${PORT}`, 'Debugger');
-
   await app.listen(PORT, () => {
     Logger.log(`Api Server is listening on port ${PORT}`, 'Bootstrap');
   });
