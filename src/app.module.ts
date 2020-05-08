41f9c2c9aa5111b5af9a8bef414a54dde7db0cbb
Oleg Babenko <majerkravchuk@gmail.com>
2020-05-08 21:50:22 +0300

    Use Redis to store the session

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SessionModule } from 'nestjs-session';
+import { SessionModule, NestSessionOptions } from 'nestjs-session';
+import * as redis from 'redis';
+import * as expressSession from 'express-session';
+import * as connectRedis from 'connect-redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -8,11 +11,23 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    SessionModule.forRoot({
-      session: {
-        secret: process.env.SESSION_SECRET,
-        resave: true,
-        saveUninitialized: true,
+    SessionModule.forRootAsync({
+      useFactory: async (): Promise<NestSessionOptions> => {
+        const RedisStore = connectRedis(expressSession);
+        const redisClient = redis.createClient({
+          url: process.env.REDIS_URL,
+        });
+
+        return {
+          session: {
+            store: new RedisStore({
+              client: redisClient,
+            }),
+            secret: process.env.SESSION_SECRET,
+            resave: true,
+            saveUninitialized: true,
+          },
+        };
       },
     }),
     AuthModule,
